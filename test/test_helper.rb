9c74ddbbc7c3aa348c5382cbc8c17cce8e0ef297
hwngcm <hwngcm@gmai.com>
2018-04-12 04:13:35 +0700

    End chap 5

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,14 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  # def full_title(page_title = '')
+  #   base_title = "Ruby on Rails Tutorial Sample App"
+  #   if page_title.empty?
+  #     base_title
+  #   else
+  #     "#{page_title} | #{base_title}"
+  #   end
+  # end  
+
   # Add more helper methods to be used by all tests here...
 end
