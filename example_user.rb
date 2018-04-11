99a483f4cb93f30332b5466e067270289897c445
hwngcm <hwngcm@gmai.com>
2018-04-12 05:29:18 +0700

    add chap 4

--- example_user.rb
+++ example_user.rb
@@ -1,14 +1,25 @@
-class User
-    attr_accessor :name, :email
+  class User
+    attr_accessor :first, :last, :full_name, :email
   
     def initialize(attributes = {})
-      @name  = attributes[:name]
+      @first  = attributes[:first]
+      @last  = attributes[:last]
       @email = attributes[:email]
+      @full_name = full_name
     end
   
     def formatted_email
-      "#{@name} <#{@email}>"
+      "#{@full_name} <#{@email}>"
+    end
+    
+    def full_name
+      "#{@first} #{@last}"
     end
 
-   
+    class User
+      def alphabetical_name
+        "#{@last}, #{@first}"
+      end
+    end
+  
   end
