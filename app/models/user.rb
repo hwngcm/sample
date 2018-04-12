e0e60894411d5ae6f2b356f4a5c1c001fc78ad2f
hwngcm <hwngcm@gmai.com>
2018-04-13 00:10:21 +0700

    end chap 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password     
-    validate:password, presence: true, length: {minimum: 6}           
+    validates:password, presence: true, length: {minimum: 6}           
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,25 +10,36 @@ class UserTest < ActiveSupport::TestCase
                     password: "foobar", password_confirmation: "foobar")
   end
 
-  # test "user valid" do
-  #   assert @user.valid?
-  # end
+  test "user valid" do
+    assert @user.valid?
+  end
 
-  # test "user name valid" do
-  #   @user.name = ""
-  #   assert_not @user.valid?
-  # end
+  test "user name presence" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
 
-  # test "user email valid" do
-  #   @user.email = ""
-  #   assert_not @user.valid?
-  # end
+  test "user email presence" do
+    @user.email = ""
+    assert_not @user.valid?
+  end
 
-  # test "email addresses should be unique" do
-  #   duplicate_user = @user.dup
-  #   duplicate_user.email = @user.email.upcase
-  #   @user.save
-  #   assert_not duplicate_user.valid?
-  # end
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 
 end
