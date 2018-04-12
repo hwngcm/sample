f651c0ce96f6f067d37517fc28820287bafb9179
hwngcm <hwngcm@gmai.com>
2018-04-12 16:09:57 +0700

    end chap 6

---
+++ test/models/user_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = User.new(name: "Hung", email: "hung@gmail.com", 
+                    password: "foobar", password_confirmation: "foobar")
+  end
+
+  # test "user valid" do
+  #   assert @user.valid?
+  # end
+
+  # test "user name valid" do
+  #   @user.name = ""
+  #   assert_not @user.valid?
+  # end
+
+  # test "user email valid" do
+  #   @user.email = ""
+  #   assert_not @user.valid?
+  # end
+
+  # test "email addresses should be unique" do
+  #   duplicate_user = @user.dup
+  #   duplicate_user.email = @user.email.upcase
+  #   @user.save
+  #   assert_not duplicate_user.valid?
+  # end
+
+end
