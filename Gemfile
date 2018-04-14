6910572b8817121f316651621e36d6d0dea86374
hwngcm <hwngcm@gmai.com>
2018-04-15 04:14:30 +0700

    end chap10

--- Gemfile
+++ Gemfile
@@ -10,6 +10,11 @@ end
 gem 'rails', '~> 5.1.6'
 gem 'bootstrap-sass', '3.3.7'
 gem 'jquery-rails'
+# tao user mau
+gem 'faker', '1.7.3'
+gem 'will_paginate', '3.1.6'
+
+
 # for digest_password
 gem 'bcrypt',         '3.1.11'
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+
   def show
     @user = User.find_by_id(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
@@ -20,7 +29,38 @@ class UsersController < ApplicationController
     end  
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Pls log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)  
+  end
+
+  # phan trang
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  # xoa user
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  
 end
